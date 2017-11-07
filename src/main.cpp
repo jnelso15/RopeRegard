ec41c452c0e7a95eda65f15f739a37fe7be71a27
Brian Nelson <jeremynelson5321@gmail.com>
2017-11-06 22:24:44 -0700

    Radio 1 Functioning

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,9 @@
+
+#include <SPI.h>
+#include <RH_RF69.h>
+#include <Wire.h>
 #include <Adafruit_NeoPixel.h>
+
 #ifdef __AVR__
   #include <avr/power.h>
 #endif
@@ -11,6 +16,36 @@
 #define NUMPIXELS      1
 #define kButtonOnePin  6
 
+// Change to 434.0 or other frequency, must match RX's freq!
+#define RF69_FREQ 434.0
+
+#if defined (__AVR_ATmega32U4__) // Feather 32u4 w/Radio
+  #define RFM69_CS      8
+  #define RFM69_INT     7
+  #define RFM69_RST     4
+#endif
+
+#if defined(ARDUINO_SAMD_FEATHER_M0) // Feather M0 w/Radio
+  #define RFM69_CS      8
+  #define RFM69_INT     3
+  #define RFM69_RST     4
+#endif
+
+#if defined (__AVR_ATmega328P__)  // Feather 328P w/wing
+  #define RFM69_INT     3  // 
+  #define RFM69_CS      4  //
+  #define RFM69_RST     2  // "A"
+#endif
+
+#if defined(ESP32)    // ESP32 feather w/wing
+  #define RFM69_RST     13   // same as LED
+  #define RFM69_CS      33   // "B"
+  #define RFM69_INT     27   // "A"
+#endif
+
+// Singleton instance of the radio driver
+RH_RF69 rf69(RFM69_CS, RFM69_INT);
+
 // When we setup the NeoPixel library, we tell it how many pixels, and which pin to use to send signals.
 // Note that for older NeoPixel strips you might need to change the third parameter--see the strandtest
 // example for more information on possible values.
@@ -19,8 +54,39 @@ Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ80
 int delayval = 500; // delay for half a second
 
 void setup() {
+    Serial.begin(115200);
+    while (!Serial) { delay(1); } // wait until serial console is open, remove if not tethered to computer
+  
   pixels.begin(); // This initializes the NeoPixel library.
   pinMode(kButtonOnePin, INPUT);
+
+  Serial.println("Feather RFM69 RX/TX Test!");
+  
+    // manual reset
+    digitalWrite(RFM69_RST, HIGH);
+    delay(10);
+    digitalWrite(RFM69_RST, LOW);
+    delay(10);
+    
+    if (!rf69.init()) {
+      Serial.println("RFM69 radio init failed");
+      while (1);
+    }
+    Serial.println("RFM69 radio init OK!");
+    
+    // Defaults after init are 434.0MHz, modulation GFSK_Rb250Fd250, +13dbM (for low power module)
+    // No encryption
+    if (!rf69.setFrequency(RF69_FREQ)) {
+      Serial.println("setFrequency failed");
+    }
+    // The encryption key has to be the same as the one in the server
+  uint8_t key[] = { 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08,
+    0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08};
+rf69.setEncryptionKey(key);
+
+Serial.print("RFM69 radio @");  Serial.print((int)RF69_FREQ);  Serial.println(" MHz");
+
+  
 }
 
 void loop() {
@@ -36,4 +102,34 @@ void loop() {
    
        pixels.show(); // This sends the updated pixel color to the hardware.
   }
+  if (rf69.waitAvailableTimeout(100)) {
+    // Should be a message for us now   
+    uint8_t buf[RH_RF69_MAX_MESSAGE_LEN];
+    uint8_t len = sizeof(buf);
+    
+    if (! rf69.recv(buf, &len)) {
+      Serial.println("Receive failed");
+      return;
+    }
+
+    rf69.printBuffer("Received: ", buf, len);
+    buf[len] = 0;
+    
+    Serial.print("Got: "); Serial.println((char*)buf);
+    Serial.print("RSSI: "); Serial.println(rf69.lastRssi(), DEC);
+  }
+
+  if (digitalRead(kButtonOnePin))
+  {
+    Serial.println("Button pressed!");
+    
+    char radiopacket[20] = "Button #";
+    if (digitalRead(kButtonOnePin)) radiopacket[8] = 'A';
+    radiopacket[9] = 0;
+
+    Serial.print("Sending "); Serial.println(radiopacket);
+    rf69.send((uint8_t *)radiopacket, strlen(radiopacket));
+    rf69.waitPacketSent();
+  }
+
 }
