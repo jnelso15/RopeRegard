b203eae86f47edddce2c5355defcd0fb0d343d39
aaronp13 <30845779+aaronp13@users.noreply.github.com>
2017-11-08 10:07:09 -0700

    Updated for allowing all of the buttons and 2 LEDs

--- src/main.cpp
+++ src/main.cpp
@@ -13,18 +13,26 @@
 #define PIN            5
 
 // How many NeoPixels are attached to the Arduino?
-#define NUMPIXELS      1
-#define kButtonOnePin  A2
+#define NUMPIXELS      2
+
+
+#define kButtonOnePin   A2
+#define kButtonTwoPin   A3
+#define kButtonThreePin A4
+#define kButtonFourPin  A5
 
 // Change to 434.0 or other frequency, must match RX's freq!
 #define RF69_FREQ 434.0
 
+//ADAFRUIT FEATHER
 /*#if defined (__AVR_ATmega32U4__) // Feather 32u4 w/Radio
   #define RFM69_CS      8
   #define RFM69_INT     7
   #define RFM69_RST     4
 #endif
 */
+
+//FEATHERWING
 #define RFM69_CS  10   // "B"
 #define RFM69_RST 11   // "A"
 #define RFM69_INT 2    // "SDA" (only SDA/SCL/RX/TX have IRQ!)
@@ -69,6 +77,8 @@ void setup() {
   uint8_t key[] = { 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08,
     0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08};
 rf69.setEncryptionKey(key);
+
+// Antenna Power Level
 rf69.setTxPower(20, true);
 
 Serial.print("RFM69 radio @");  Serial.print((int)RF69_FREQ);  Serial.println(" MHz");
@@ -79,14 +89,69 @@ Serial.print("RFM69 radio @");  Serial.print((int)RF69_FREQ);  Serial.println(" 
 void loop() {
   // For a set of NeoPixels the first NeoPixel is 0, second is 1, all the way up to the count of pixels minus one.
 
+  //TRANSMITER
   if(digitalRead(kButtonOnePin)) {
-    pixels.setPixelColor(0, pixels.Color(0,150,0)); // Moderately bright green color.
+
+    Serial.println("Button A pressed!");
     
-        pixels.show(); // This sends the updated pixel color to the hardware.
+    char radiopacket[20] = "Button #";
+    radiopacket[8] = 'A';
+    radiopacket[9] = 0;
+
+    Serial.print("Sending "); Serial.println(radiopacket);
+    rf69.send((uint8_t *)radiopacket, strlen(radiopacket));
+    rf69.waitPacketSent();
+
+
+    pixels.setPixelColor(0, pixels.Color(150,0,100)); // Purple
+    pixels.show(); // This sends the updated pixel color to the hardware.
+  } else if (digitalRead(kButtonTwoPin)) {
+    Serial.println("Button B pressed!");
+    
+    char radiopacket[20] = "Button #";
+    radiopacket[8] = 'B';
+    radiopacket[9] = 0;
+
+    Serial.print("Sending "); Serial.println(radiopacket);
+    rf69.send((uint8_t *)radiopacket, strlen(radiopacket));
+    rf69.waitPacketSent();
+
+    pixels.setPixelColor(1, pixels.Color(0,66,37)); // British Racing Green
+    pixels.show(); // This sends the updated pixel color to the hardware.
+    delay(delayval);
+  } else if (digitalRead(kButtonTwoPin)) {
+    Serial.println("Button C pressed!");
+    
+    char radiopacket[20] = "Button #";
+    radiopacket[8] = 'C';
+    radiopacket[9] = 0;
+
+    Serial.print("Sending "); Serial.println(radiopacket);
+    rf69.send((uint8_t *)radiopacket, strlen(radiopacket));
+    rf69.waitPacketSent();
+
+    pixels.setPixelColor(1, pixels.Color(255,246,0)); // Yellow.
+    pixels.show(); // This sends the updated pixel color to the hardware.
+    delay(delayval);
+  } else if (digitalRead(kButtonTwoPin)) {
+    Serial.println("Button D pressed!");
+    
+    char radiopacket[20] = "Button #";
+    radiopacket[8] = 'D';
+    radiopacket[9] = 0;
+
+    Serial.print("Sending "); Serial.println(radiopacket);
+    rf69.send((uint8_t *)radiopacket, strlen(radiopacket));
+    rf69.waitPacketSent();
+
+    pixels.setPixelColor(1, pixels.Color(42,82,190)); // Moderately bright green color.
+    pixels.show(); // This sends the updated pixel color to the hardware.
+    delay(delayval);
   } else {
-    pixels.setPixelColor(0, pixels.Color(0,0,150)); // Moderately bright green color.
     
-        pixels.show(); // This sends the updated pixel color to the hardware.
+
+    pixels.setPixelColor(0, pixels.Color(0,0,0)); // OFF
+    pixels.show(); // This sends the updated pixel color to the hardware.
   }
   
   if (rf69.waitAvailableTimeout(100)) {
@@ -96,6 +161,8 @@ void loop() {
     
 
     
+
+    //RECIEVE
     if (! rf69.recv(buf, &len)) {
       Serial.println("Receive failed");
       return;
@@ -106,29 +173,26 @@ void loop() {
     
     Serial.print("Got: "); Serial.println((char*)buf);
     if((String((char*)buf)).indexOf("A") != -1) {
-      pixels.setPixelColor(0, pixels.Color(150,0,100)); // Moderately bright green color.
-      
+      pixels.setPixelColor(0, pixels.Color(150,0,100)); // Purple
+      pixels.show(); // This sends the updated pixel color to the hardware.
+      delay(delayval);
+    } else if((String((char*)buf)).indexOf("B") != -1) {
+      pixels.setPixelColor(1, pixels.Color(0,66,37)); // British Racing Green
+      pixels.show(); // This sends the updated pixel color to the hardware.
+      delay(delayval);
+    } else if((String((char*)buf)).indexOf("C") != -1) {
+      pixels.setPixelColor(1, pixels.Color(255,246,0)); // Yellow.
+      pixels.show(); // This sends the updated pixel color to the hardware.
+      delay(delayval);
+    } else if((String((char*)buf)).indexOf("D") != -1) {
+      pixels.setPixelColor(1, pixels.Color(42,82,190)); // Moderately bright green color.
       pixels.show(); // This sends the updated pixel color to the hardware.
       delay(delayval);
     } else {
-      pixels.setPixelColor(0, pixels.Color(0,0,150)); // Moderately bright green color.
-      
-          pixels.show(); // This sends the updated pixel color to the hardware.
+      pixels.setPixelColor(1, pixels.Color(0,0,0)); // OFF
+      pixels.show(); // This sends the updated pixel color to the hardware.
     }
     Serial.print("RSSI: "); Serial.println(rf69.lastRssi(), DEC);
-  }
-
-  if (digitalRead(kButtonOnePin))
-  {
-    Serial.println("Button pressed!");
-    
-    char radiopacket[20] = "Button #";
-    if (digitalRead(kButtonOnePin)) radiopacket[8] = 'A';
-    radiopacket[9] = 0;
-
-    Serial.print("Sending "); Serial.println(radiopacket);
-    rf69.send((uint8_t *)radiopacket, strlen(radiopacket));
-    rf69.waitPacketSent();
   }
 
 }
